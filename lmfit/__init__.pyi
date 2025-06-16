28ff357f80b5937a692c589e6d32945a719b67a9
Corenthin ZOZOR <corenthin.zozor@gmail.com>
2025-06-16 13:22:20 +0200

    Explicit exports on __init__.pyi

--- lmfit/__init__.pyi
+++ lmfit/__init__.pyi
@@ -1,7 +1,6 @@
-from . import lineshapes, models
-from .confidence import conf_interval, conf_interval2d
-from .minimizer import Minimizer, MinimizerException, minimize
-from .model import CompositeModel, Model
-from .parameter import Parameter, Parameters, create_params
-from .printfuncs import ci_report, fit_report, report_ci, report_fit
-from .version import version as __version__
+from .confidence import conf_interval as conf_interval, conf_interval2d as conf_interval2d
+from .minimizer import Minimizer as Minimizer, MinimizerException as MinimizerException, minimize as minimize
+from .model import CompositeModel as CompositeModel, Model as Model
+from .parameter import Parameter as Parameter, Parameters as Parameters, create_params as create_params
+from .printfuncs import ci_report as ci_report, fit_report as fit_report, report_ci as report_ci, \
+    report_fit as report_fit
